e3a8a0d62ed26d79ba181a668400f608fe9677a2
Anil Kumar Krishnashetty <anil@brightdata.com>
2025-05-16 10:48:19 +0200

    Update hello world example

--- hellowrold.js
+++ hellowrold.js
@@ -4,26 +4,34 @@ const BROWSER_WS_ENDPOINT = "BROWSER_WS_ENDPOINT";
   
 (async () => {  
     console.log('Connecting to Scraping Browser...');  
-    const browser = await puppeteer.connect({  
-        browserWSEndpoint: BROWSER_WS_ENDPOINT,  
-   });  
+    let browser;
     try {  
+        browser = await puppeteer.connect({  
+            browserWSEndpoint: BROWSER_WS_ENDPOINT,  
+        });  
         console.log('Connected! Navigating...');  
         const page = await browser.newPage();  
         // Enter your test URL below
         await page.goto('https://example.com', { timeout: 2 * 60 * 1000 });  
         console.log('Taking screenshot to page.png');  
         await page.screenshot({ path: './page.png', fullPage: true });  
-   console.log('Navigated! Scraping page content...');  
- const html = await page.content();  
- console.log(html)  
- // CAPTCHA solving: If you know you are likely to encounter a CAPTCHA on your target page, add the following few lines of code to get the status of Scraping Browser's automatic CAPTCHA solver   
- // Note 1: If no captcha was found it will return not_detected status after detectTimeout   
- // Note 2: Once a CAPTCHA is solved, if there is a form to submit, it will be submitted by default   
- // const client = await page.target().createCDPSession();  
- // const {status} = await client.send('Captcha.solve', {detectTimeout: 30*1000});   
- // console.log(`Captcha solve status: ${status}`)   
+        console.log('Navigated! Scraping page content...');  
+        const html = await page.content();  
+        console.log(html);  
+        // CAPTCHA solving: If you know you are likely to encounter a CAPTCHA on your target page, add the following few lines of code to get the status of Scraping Browser's automatic CAPTCHA solver   
+        // Note 1: If no captcha was found it will return not_detected status after detectTimeout   
+        // Note 2: Once a CAPTCHA is solved, if there is a form to submit, it will be submitted by default   
+        // const client = await page.target().createCDPSession();  
+        // const {status} = await client.send('Captcha.solve', {detectTimeout: 30*1000});   
+        // console.log(`Captcha solve status: ${status}`);   
+    } catch (error) {
+        console.error('An error occurred during scraping:');
+        console.error(error.message);
+        console.error(error.stack);
     } finally {  
-        await browser.close();  
-   }  
+        if (browser) {
+            await browser.close();
+            console.log('Browser closed.');
+        }
+    }  
 })();
